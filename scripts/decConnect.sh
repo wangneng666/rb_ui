bcea16d5cd9009250ae76851f3886b9bdd0273ea
wn <wb@123.com>
2020-06-09 10:22:23 +0800

    0609系统自检功能测试

--- scripts/decConnect.sh
+++ scripts/decConnect.sh
@@ -4,6 +4,9 @@ roslaunch gripper_bridge gripper_bridge_dual.launch &
 roslaunch realsense2_camera rs_camera_right.launch &
 roslaunch realsense2_camera rs_camera.launch &
 roslaunch hsr_bringup publish_d435i_calibration_dual.launch &
-sleep 0.5
+roslaunch vision_bridge vision_bridge_yolo6d_dual.launch &
+roslaunch grasp_place grasp.launch &
+roslaunch rubik_cube_solve solve.launch &
+rosrun cubeParse cube &
 wait
 exit 0
